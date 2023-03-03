96bedfc15aecf3fe79d06c68f6c70ebc2e7981fe
Your Name <you@example.com>
2023-03-03 14:51:28 +0700

    lab23 complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    if(argc==1)
+        cout<<"Please input numbers to find average.";
+    else{
+        float sum{};
+        for (int i = 1; i < argc; i++){
+            sum+=atof(argv[i]);
+        }
+        cout<<"---------------------------------\n";
+        cout<<"Average of "<<argc-1<<" numbers = "<<sum/(float)(argc-1);
+        cout<<"\n---------------------------------\n";
+    }
+    return 0;
+}
 
